f442ef95398bf088847bb81c118e374304d2c0bb
ariezieq <160419356+ariezieq@users.noreply.github.com>
2024-02-19 00:49:32 +0700

    Add files via upload

---
+++ suhu.py
@@ -0,0 +1,9 @@
+reamur = float(input("masukan suhu reamur: "))
+
+celcius = (5/4) * reamur
+fahrenheit = (9/4) * reamur + 32
+kelvin = (5 / 4) * reamur + 273.15
+
+print("konversi ke celcius: %.2f" % celcius)
+print("konversi ke fahrenheit: %.2f" % fahrenheit)
+print("konversi ke kelvin: %.2f" % kelvin)       
---
+++ umur.py
@@ -0,0 +1,6 @@
+tahunlahir = float(input("masukan tahun lahir kamu:"))
+tahunsekarang = float(input("masukan tahun sekarang:"))
+
+umur = tahunsekarang-tahunlahir
+
+print("umur anda adalah: %.of tahun" % umur )
